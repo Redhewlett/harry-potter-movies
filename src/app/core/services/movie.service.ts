3e3a7a3300983946c6401ad72a6970a2b749a497
``teddy`` <``edmondteddy@hotmail.fr``>
2024-04-23 11:39:17 +0200

    Add MovieService and Movie interface

--- src/app/core/services/movie.service.ts
+++ src/app/core/services/movie.service.ts
@@ -1,17 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { pipe, shareReplay } from 'rxjs';
+import { Movie } from '../../shared/interfaces/movie';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
-  // share replay
+
   public movies$ = this.getMovies().pipe(shareReplay(1))
 
   constructor(private http:HttpClient) {}
 
   private getMovies() {
-    return this.http.get('/movies');
+    return this.http.get<Movie>('/movies');
   }
 }
---
+++ src/app/shared/interfaces/movie.ts
@@ -0,0 +1,7 @@
+export interface Movie {
+  id: string;
+  budget: number;
+  duration: number;
+  release_date: string;
+  title: string;
+}
