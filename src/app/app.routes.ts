57fe567231eec83907c8bcb3b29c9ba5e96a9d71
``teddy`` <``edmondteddy@hotmail.fr``>
2024-04-23 17:25:30 +0200

    Update movies module and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,19 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '',
-    loadChildren: () => import('./modules/movies/movies.module').then(m => m.MoviesModule),
+  {
+    path: '',
+    loadChildren: () =>
+      import('./pages/movies/movies.module').then((m) => m.MoviesModule),
+  },
+  {
+    path: 'movies/:movieId',
+    loadChildren: () =>
+      import('./pages/movies/movies.module').then((m) => m.MoviesModule),
+  },
+  // redirect to home page if route not found, bonus have a quick 404 page
+  {
+    path: '**',
+    redirectTo: '',
   },
 ];
