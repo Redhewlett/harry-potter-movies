7499407b8d5b5f1a94ad2711ed7a9ad249a5f1de
``teddy`` <``edmondteddy@hotmail.fr``>
2024-04-23 23:30:46 +0200

    Update movie-details component to display movie title dynamically

--- src/app/pages/movies/movie-details/movie-details.component.ts
+++ src/app/pages/movies/movie-details/movie-details.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MovieService } from '../../../core/services/movie.service';
+import { ActivatedRoute } from '@angular/router';
+import { Movie } from '../../../shared/models/movie.model';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-movie-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './movie-details.component.html',
-  styleUrl: './movie-details.component.css'
+  styleUrl: './movie-details.component.css',
 })
-export class MovieDetailsComponent {
+export class MovieDetailsComponent implements OnInit, OnDestroy {
+  public movie: Movie | undefined;
+  private subscription: Subscription | undefined;
 
+  constructor(
+    private route: ActivatedRoute,
+    public movieService: MovieService
+  ) {}
+
+  ngOnInit() {
+    let id: string | undefined;
+    this.route.params.subscribe((value) => {
+      id = value['movieId'];
+    });
+    if (id) {
+      const subscription = this.movieService.getMovie(id).subscribe((movie) => {
+        this.movie = movie;
+      });
+      // lets store the subscription to unsubscribe later
+      this.subscription = subscription;
+    }
+  }
+
+  ngOnDestroy(): void {
+    //lets unsubscribe to avoid memory leaks
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
 }
