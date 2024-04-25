74d423e7f8c436bfae5ef27f3f36240758cbcf03
teddy edmond <92273094+Redhewlett@users.noreply.github.com>
2024-04-25 22:48:57 +0200

    Update movie-filter component to add filter functionality

--- src/app/pages/movies/components/movie-filter/movie-filter.component.ts
+++ src/app/pages/movies/components/movie-filter/movie-filter.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, signal } from '@angular/core';
+import { MovieService } from '../../../../core/services/movie.service';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-movie-filter',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './movie-filter.component.html',
-  styleUrl: './movie-filter.component.css'
+  styleUrl: './movie-filter.component.css',
 })
 export class MovieFilterComponent {
+  //the two form inputs
+  public title = signal('');
+  public releaseYear = signal(0);
 
+  constructor(private movieService: MovieService) {}
 }
--- src/app/pages/movies/movies.component.ts
+++ src/app/pages/movies/movies.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { MovieService } from '../../core/services/movie.service';
 import { MovieItemComponent } from './components/movie-item/movie-item.component';
 import { CommonModule } from '@angular/common';
+import { MovieFilterComponent } from './components/movie-filter/movie-filter.component';
 @Component({
   selector: 'app-movies',
   standalone: true,
-  imports: [MovieItemComponent, CommonModule],
+  imports: [MovieItemComponent, CommonModule, MovieFilterComponent],
   templateUrl: './movies.component.html',
   styleUrl: './movies.component.css',
 })
