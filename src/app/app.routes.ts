5ffc3587bf353e96258df18e21c87153072ee76d
teddy edmond <92273094+Redhewlett@users.noreply.github.com>
2024-04-26 19:40:52 +0200

    Refactor routing in app.routes.ts and remove movies.module.ts and movies.routing.module.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,13 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadChildren: () =>
-      import('./pages/movies/movies.module').then((m) => m.MoviesModule),
+    loadComponent: () =>
+      import('./pages/movies/movies.component').then((m) => m.MoviesComponent),
   },
   {
     path: 'movies/:movieId',
-    loadChildren: () =>
-      import('./pages/movies/movies.module').then((m) => m.MoviesModule),
+    loadComponent: () =>
+      import('./pages/movies/movie-details/movie-details.component').then((m) => m.MovieDetailsComponent),
   },
   // redirect to home page if route not found, bonus have a quick 404 page
   {
--- src/app/pages/movies/movies.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { MoviesRoutingModule } from './movies.routing.module';
-
-@NgModule({
-  declarations: [],
-  imports: [MoviesRoutingModule],
-})
-export class MoviesModule {}
